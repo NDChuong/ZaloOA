58ef17fd15b861c40db82cea8b217b8c66bf10bb
NguyenDinhChuong <nguyendinhchuong2607@gmail.com>
2018-07-16 00:50:39 +0700

    del sckey

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ var zaConfig = {
 var ZOAClient = new ZaloOA(zaConfig);
 //console.log(ZOAClient);
 
-var oaId = '53425692806963372';
-var sk = 'ZET7wmwkMDVk41YqnQKW';
-// var uid = 841635252711;
 
-// var url = 'https://openapi.zaloapp.com/oa/v1/getfollowers';
-var userId = '841635252711';
+
+
+
+
 var data = { "offset": 0, "count": 50 };
 
 // console.log(Date.now());
@@ -50,9 +49,9 @@ ZOAClient.api('getfollowers', { oaid: oaId, data: data, timestamp: Date.now(), m
                 uid: entry.uid,
                 actionlist: [{
                     action: 'oa.open.inapp',
-                    title: 'VNG Strategy Forum -  August 18-20, 2017',
-                    description: 'Anh/chị vui lòng tập trung vào lúc 6:00AM tại sảnh tòa nhà Flemington. BTC sẽ check-in, hỗ trợ di chuyển hành lý và xe sẽ khởi hành đi The Grand Ho Tram Strip vào lúc 6:30AM. BTC có chuẩn bị phần ăn sáng cho anh/chị tại Mekong Rest Stop vào lúc 7:30AM nên mọi người không cần ăn sáng trước nha. Chúc anh/chị ngủ ngon và hẹn gặp lại vào lúc 6:00AM sáng mai nhé. \nBTC,',
-                    thumb: 'https://i.imgur.com/qzsS9Ui.png',
+                    title: '',
+                    description: '',
+                    thumb: '',
                     //href: 'https://developers.zalo.me',
                    // data: 'https://developers.zalo.me',
                     // popup: {
